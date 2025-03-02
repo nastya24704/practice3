3d1c86bdb9f0e42bcb89b028def89e945e7ea167
nastya24704 <nastazeravina@gmail.com>
2025-03-03 06:29:46 +0700

    Create task13.py

---
+++ task13.py
@@ -0,0 +1,11 @@
+lines=int(input())
+columns=int(input())
+number=int(input())
+#page*cjlumns*lines= number
+page=number//columns//lines+1
+#55=(x-1)25
+column=(number-(page-1)*lines*columns)//lines+1
+#55=25+25+5=>
+#55=lines*(column-1)+line
+line=((number-(page-1)*lines*columns)-lines*(column-1))
+print('страница', page, 'столбец', column, 'строка',line)
